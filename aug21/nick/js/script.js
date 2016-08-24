33aeec9db33a9ea10c47906e0884462c80cb7502
interface-student-sites <interfaceschool@gmail.com>
2016-08-23 19:57:11 -0500

    Update script.js

--- aug21/nick/js/script.js
+++ aug21/nick/js/script.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
   });
   
   $('#fancy-text').click(function(){
-    $(this).css("font-style", "italic");
+    $(this).css({"font-style":"italic", "color":"purple", "font-size":"30px"});
     
   });
   
