08bd5d3d316ca7fa6eee03d4915f29526bb6d9e5
interface-student-sites <interfaceschool@gmail.com>
2016-10-22 12:14:50 -0500

    Update script.js

--- intro-webdev-oct19/shonna/inclass/js/script.js
+++ intro-webdev-oct19/shonna/inclass/js/script.js
@@ -2,4 +2,9 @@ $(document).ready(function () {
   $('img').click(function () {
     $(this).hide();
   });
+  
+  $('header').click(function(){
+    $(this).css("background-color", "red");
+  });
+  
 });
