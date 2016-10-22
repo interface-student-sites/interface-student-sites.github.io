daef8f32edaabe587738866f6a2acce33f4781e7
interface-student-sites <interfaceschool@gmail.com>
2016-10-22 12:17:11 -0500

    Update script.js

--- intro-webdev-oct19/kody/Inclass/js/script.js
+++ intro-webdev-oct19/kody/Inclass/js/script.js
@@ -2,7 +2,9 @@ $(document).ready(function () {
  $('img').click(function(){
  $(this).hide();
  });
- $('header').click(function(){
-  $(this).css(background-color","red");
-
+ 
+  $('header').click(function(){
+    $(this).css("background-color", "red");
+  });
+  
 });
