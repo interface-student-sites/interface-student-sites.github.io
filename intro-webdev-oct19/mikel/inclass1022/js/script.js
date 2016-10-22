51c4d9ec113d643ccba98e6589ca7b9a099bbae3
interface-student-sites <interfaceschool@gmail.com>
2016-10-22 12:14:21 -0500

    Update script.js

--- intro-webdev-oct19/mikel/inclass1022/js/script.js
+++ intro-webdev-oct19/mikel/inclass1022/js/script.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
   $('img').click(function () {
     $(this).hide();
   });
+  $(header).click(function () {
+    $('header').css("background-color", "red");
+  });
 });
 
 /*
