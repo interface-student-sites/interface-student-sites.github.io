f1b5fb2e06987a9200b85c0974bb01ef7f6aa3dc
interface-student-sites <interfaceschool@gmail.com>
2016-10-22 12:19:12 -0500

    Update script.js

--- intro-webdev-oct19/nancy/in-class/js/script.js
+++ intro-webdev-oct19/nancy/in-class/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
   }); 
 
   $('header').click(function(){
-    $('this').css("background-color", "red"); 
+    $(this).css("background-color", "red"); 
   });
   
 });
