94a1ae039844cad88eabcd9df2764d8e296422ff
interface-student-sites <interfaceschool@gmail.com>
2016-11-19 16:36:49 -0600

    Update script.js

--- nov19/Paul/js/script.js
+++ nov19/Paul/js/script.js
@@ -2,5 +2,9 @@ $(document).ready(function () {
  $("h1, p").fadeIn(5000);
  $("button").click(function(){
  $("img").toggle();
- });  
+ }); 
+ $("body".click(function() {
+  $("body").css({"background-color", "yellow"});
+  });
 });
+ 
